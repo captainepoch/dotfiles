58453e9525bcddae83c00bfa876ca378b01c02d6
Adolfo Santiago <epoch@nixnetmail.com>
2022-11-29 22:12:33 +0100

    Fix email config for Neomutt

--- emacs/mutt.el
+++ emacs/mutt.el
@@ -1,14 +1,18 @@
 (add-to-list 'load-path (concat user-emacs-directory "config"))
 (load "packages.el")
+(load "interface.el")
 (load "general.el")
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file 'noerror)
-(load "interface.el")
 ;; Delete selected text
 (delete-selection-mode 1)
 ;; Delete white space at line ending
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
-(setq-default fill-column 72)
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 (setq-default auto-fill-function 'do-auto-fill)
-(setq auto-mode-alist (append '(("/tmp/mutt.*" . mail-mode)) auto-mode-alist))
+;;(setq auto-mode-alist (append '(("/tmp/mutt.*" . mail-mode)) auto-mode-alist))
+(setq auto-mode-alist (append '(("neomutt*" . mail-mode)) auto-mode-alist))
+(setq-default fill-column 72)
+
+;; End message - OK!
+(message "Neomutt config OK")
