4151be16637911ee387473bda7bf1a6318b0dcb6
Adolfo Santiago <epoch@nixnetmail.com>
2022-12-08 08:43:00 +0100

    Format development.el

--- emacs/config/development.el
+++ emacs/config/development.el
@@ -16,18 +16,56 @@
   :ensure t
   :bind ("M-;" . comment-dwim-2))
 
-;; rainbow-delimiters - colored delimiters
-(use-package rainbow-delimiters
+;; company - auto-completion system
+(use-package company
   :ensure t
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-;; projectile - project interaction library
-(use-package projectile
-  :ensure t
+  :demand t
   :config
-  (projectile-global-mode 1)
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
-  ;;(setq projectile-switch-project-action 'neotree-projectile-action))
+  ;;(progn
+  :commands (company-complete-common
+             company-complete-common-or-cycle
+             company-manual-begin
+             company-grab-line)
+  :init
+  (setq
+   company-tooltip-limit 20
+   company-idle-delay 0.3
+   company-echo-delay 0
+   company-show-numbers t
+   company-tooltip-align-annotations t
+   company-global-modes
+   '(not erc-mode
+		 circe-mode
+		 message-mode
+		 help-mode
+		 gud-mode
+		 vterm-mode)
+   ;; These auto-complete the current selection when
+   ;; `company-auto-commit-chars' is typed. This is too magical. We
+   ;; already have the much more explicit RET and TAB.
+   company-auto-commit nil
+   company-frontends
+   '(company-pseudo-tooltip-frontend  ; always show candidates in overlay tooltip
+     company-echo-metadata-frontend)  ; show selected candidate docs in echo area
+   ;; Only search the current buffer for `company-dabbrev' (a backend that
+   ;; suggests text your open buffers). This prevents Company from causing
+   ;; lag once you have a lot of buffers open.
+   company-dabbrev-other-buffers nil
+   ;; Make `company-dabbrev' fully case-sensitive, to improve UX with
+   ;; domain-specific words with particular casing.
+   company-dabbrev-ignore-case nil
+   company-dabbrev-downcase nil)
+  (global-company-mode);;)
+  :bind (("<C-tab>" . company-complete)))
+
+;; company-quickhelp - documentation popup for company
+(use-package company-quickhelp
+  :ensure t
+  :diminish company-mode
+  :init
+  (add-hook 'global-company-mode-hook #'company-quickhelp-mode)
+  :config (company-quickhelp-mode)
+  :bind ("C-c h" . #'company-quickhelp-manual-begin))
 
 ;; eldoc - shows the argument list of the function call
 (use-package eldoc
@@ -35,14 +73,41 @@
   :demand t
   :hook (prog-mode . eldoc-mode))
 
-;; yasnippet - template system
-(use-package yasnippet
+;; flycheck - syntax checker
+(use-package flycheck
   :ensure t
-  :diminish 'yas-minor-mode
-  :demand t
+  :init
+  (add-hook 'prog-mode-hook #'flycheck-mode)
   :config
-  (add-to-list 'yas-snippet-dirs (concat user-emacs-directory "snippets"))
-  (yas-global-mode 1))
+  (set-face-underline 'flycheck-error '(:color "#dc322f" :style line)))
+
+;; git-gutter - shows git diff in the gutter
+(use-package git-gutter
+  :ensure t
+  :config
+  (global-git-gutter-mode 't)
+  :diminish git-gutter-mode)
+
+;; iedit - edit multiple regions simultaneously
+(use-package iedit
+  :ensure t
+  :bind (("C-c i" . iedit-mode)))
+
+;; indent-guide - indent block
+(use-package indent-guide
+  :ensure t
+  :init
+  (add-hook 'yaml-mode-hook 'indent-guide-mode)
+  (add-hook 'python-mode-hook 'indent-guide-mode))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  (lsp-enable-which-key-integration t)
+  :hook
+  ((go-mode) . lsp))
 
 ;; magit - git interface for emacs
 (use-package magit
@@ -54,77 +119,6 @@
   :bind (("C-x g" . magit-status))
   :custom (git-commit-summary-max-length 50))
 
-;; git-gutter - shows git diff in the gutter
-(use-package git-gutter
-  :ensure t
-  :config
-  (global-git-gutter-mode 't)
-  :diminish git-gutter-mode)
-
-;; flycheck - syntax checker
-(use-package flycheck
-  :ensure t
-  :init
-  (add-hook 'prog-mode-hook #'flycheck-mode)
-  :config
-  (set-face-underline 'flycheck-error '(:color "#dc322f" :style line)))
-
-;; iedit - edit multiple regions simultaneously
-(use-package iedit
-  :ensure t
-  :bind (("C-c i" . iedit-mode)))
-
-;; company - auto-completion system
-(use-package company
-  :ensure t
-  :demand t
-  :config
-  ;;(progn
-    :commands (company-complete-common
-             company-complete-common-or-cycle
-             company-manual-begin
-             company-grab-line)
-  :init
-	(setq
-	 company-tooltip-limit 20
-	 company-idle-delay 0.3
-	 company-echo-delay 0
-	 company-show-numbers t
-	 company-tooltip-align-annotations t
-   company-global-modes
-    '(not erc-mode
-      circe-mode
-      message-mode
-      help-mode
-      gud-mode
-      vterm-mode)
-	 ;; These auto-complete the current selection when
-   ;; `company-auto-commit-chars' is typed. This is too magical. We
-   ;; already have the much more explicit RET and TAB.
-   company-auto-commit nil
-	 company-frontends
-     '(company-pseudo-tooltip-frontend  ; always show candidates in overlay tooltip
-       company-echo-metadata-frontend)  ; show selected candidate docs in echo area
-        ;; Only search the current buffer for `company-dabbrev' (a backend that
-        ;; suggests text your open buffers). This prevents Company from causing
-        ;; lag once you have a lot of buffers open.
-        company-dabbrev-other-buffers nil
-        ;; Make `company-dabbrev' fully case-sensitive, to improve UX with
-        ;; domain-specific words with particular casing.
-        company-dabbrev-ignore-case nil
-        company-dabbrev-downcase nil)
-	(global-company-mode);;)
-  :bind (("<C-tab>" . company-complete)))
-
-;; company-quickhelp - documentation popup for company
-(use-package company-quickhelp
-  :ensure t
-  :diminish company-mode
-  :init
-    (add-hook 'global-company-mode-hook #'company-quickhelp-mode)
-  :config (company-quickhelp-mode)
-  :bind ("C-c h" . #'company-quickhelp-manual-begin))
-
 ;; mwim -- move to the beginning/end code line
 (use-package mwim
   :ensure t
@@ -132,9 +126,24 @@
   ("C-a" . mwim-beginning-of-code-or-line)
   ("C-e" . mwim-end-of-code-or-line))
 
-;; indent-guide - indent block
-(use-package indent-guide
+;; projectile - project interaction library
+(use-package projectile
   :ensure t
-  :init
-    (add-hook 'yaml-mode-hook 'indent-guide-mode)
-    (add-hook 'python-mode-hook 'indent-guide-mode))
+  :config
+  (projectile-global-mode 1)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+;;(setq projectile-switch-project-action 'neotree-projectile-action))
+
+;; rainbow-delimiters - colored delimiters
+(use-package rainbow-delimiters
+  :ensure t
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; yasnippet - template system
+(use-package yasnippet
+  :ensure t
+  :diminish 'yas-minor-mode
+  :demand t
+  :config
+  (add-to-list 'yas-snippet-dirs (concat user-emacs-directory "snippets"))
+  (yas-global-mode 1))
