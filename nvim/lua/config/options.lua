b6ae90833c93e2ebd91c5caaa73e93ed954c33ed
Adolfo Santiago <epoch@adol.pw>
2023-07-14 18:19:35 +0200

    Some vimrc to nvim config variables

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,4 +1,24 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
+
+-- Encoding
+vim.opt.encoding = "utf-8"
+vim.opt.fileformat = "unix"
+vim.opt.fileformats = { "unix" }
+vim.opt.fileencodings = { "utf-8", "ucs-bom", "latin1" }
+
+vim.opt.breakindent = true
+vim.opt.clipboard = { "unnamedplus" }
+vim.opt.linebreak = true
+vim.opt.scrolloff = 3
+vim.opt.shiftwidth = 4
+vim.opt.sidescroll = 3
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+
+vim.opt.lbr = true
+
+vim.opt.textwidth = 80
+vim.opt.wrapmargin = 2
+vim.opt.cc = { 80 }
