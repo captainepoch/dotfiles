b738c079d49b72b4184bfd66e69f1d31eff1e181
Adolfo Santiago <epoch@adol.pw>
2023-07-15 07:44:47 +0200

    Add more default options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -10,12 +10,14 @@ vim.opt.fileencodings = { "utf-8", "ucs-bom", "latin1" }
 
 vim.opt.breakindent = true
 vim.opt.clipboard = { "unnamedplus" }
+vim.opt.expandtab = false
 vim.opt.linebreak = true
 vim.opt.scrolloff = 3
 vim.opt.shiftwidth = 4
 vim.opt.sidescroll = 3
 vim.opt.softtabstop = 4
 vim.opt.tabstop = 4
+vim.opt.wrap = true
 
 vim.opt.lbr = true
 
