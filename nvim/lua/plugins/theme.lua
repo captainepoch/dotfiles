2b5ca539be8fe59a376dc8090a3f0e6fa25bb358
Adolfo Santiago <epoch@adol.pw>
2025-12-22 07:19:13 +0100

    Small adjustements for nvim

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,36 +1,38 @@
 return {
-  -- "catppuccin/nvim",
-  -- name = "catppuccin",
-  -- priority = 1000,
-  -- opts = {
-  --   flavour = "mocha",
-  --   integrations = {
-  --     cmp = true,
-  --     gitsigns = true,
-  --     nvimtree = true,
-  --     treesitter = true,
-  --     telescope = true,
-  --     mason = true,
-  --     which_key = true,
-  --     native_lsp = {
-  --       enabled = true,
-  --     },
-  --   },
-  -- },
   {
-    "navarasu/onedark.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
-    config = function()
-      require("onedark").setup({
-        style = "cool",
-      })
-      require("onedark").load()
-    end,
+    opts = {
+      flavour = "frappe",
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        telescope = true,
+        mason = true,
+        which_key = true,
+        native_lsp = {
+          enabled = true,
+        },
+      },
+    },
   },
+  -- {
+  --   "navarasu/onedark.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     require("onedark").setup({
+  --       style = "cool",
+  --     })
+  --     require("onedark").load()
+  --   end,
+  -- },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "catppuccin",
     },
   },
 }
